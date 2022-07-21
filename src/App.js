40b76fa14d014eaeea04892c8e311e34ac714261
chanakanch61 <96366006+chanakanch61@users.noreply.github.com>
2022-07-21 22:27:58 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Signin from './Signin';
-import Profile from './Profile';
 
 function App() {
-  const token = localStorage.clear();
-
-  if(!token) {
-    return <Signin />
-  }
-
   return (
-    <div className="wrapper">
-      <Profile />
-      <BrowserRouter>
-        <Route>
-          <Link path="/">
-            <Profile />
-          </Link>
-          <Link path="/profile">
-            <Profile />
-          </Link>
-        </Route>
-      </BrowserRouter>
+    <div>
+      <Signin />
     </div>
+
   );
 }
 
